b2bee55252195e4726dba997ad71a4ffc5ec3d28
Haiming Gao <gao.haiming97@gmail.com>
2017-10-22 04:11:07 -0500

    iasdjflkasjdfkl

--- addEl.js
+++ addEl.js
@@ -1,14 +1,59 @@
 function passData(text)
 {
-    if(text == "") return;
-    var xhr = new XMLHttpRequest();
+    var trimText = text.value.trim();
+    var params = {
+        botAlias: 'CustoServe',
+        botName: 'CustoServe',
+        inputText: trimText,
+        userId: lexUserId,
+        sessionAttributes: sessionAttributes
+    };
+    lexruntime.postText(params, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            showError('Error:  ' + err.message + ' (see console for details)')
+        }
+        if (data) {
+            // capture the sessionAttributes for the next cycle
+            sessionAttributes = data.sessionAttributes;
+            // show response and/or error/dialog status
+            showResponse(data);
+        }
+        // re-enable input
+        wisdomText.value = '';
+        wisdomText.locked = false;
+    });
+    /*var xhr = new XMLHttpRequest();
     var url ="https://runtime.lex.us-east-1.amazonaws.com/bot/CustoServe/alias/CustoServe/user/demo_user/text";
     xhr.open("POST", url, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify({
         "inputText": text
-    }));
-    
+    }));*/
+}
+
+function showResponse(lexResponse) {
+    var div = document.createElement("DIV");
+    var content = document.createElement("DIV")
+    div.className = "user"; 
+    content.className = "chatblock-bot";
+    var t;
+    if (lexResponse.message) {
+        t = document.createTextNode(lexResponse.message);
+    }
+    if (lexResponse.dialogState === 'ReadyForFulfillment') {
+        t = document.createTextNode('Ready for fulfillment');
+        // TODO:  show slot values
+    } else {
+        t = document.createTextNode('(' + lexResponse.dialogState + ')');
+    }
+    div.appendChild(t);      
+    content.appendChild(img);                     
+    content.appendChild(div);
+    chatWindow = document.getElementById("chat");
+    chatWindow.appendChild(content); 
+    chatWindow.scrollTop = chatWindow.scrollHeight;
+    document.getElementById("textbox").value = "";     
 }
 
 function addUserText() {

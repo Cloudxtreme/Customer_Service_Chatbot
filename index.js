1e6424d15dc9f213dabee9290f981da28f86c03c
Sriram Bhat <srirambhat7@gmail.com>
2017-10-21 22:34:17 -0500

    express static added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use("/", express.static(__dirname));
 
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/chat.html');
